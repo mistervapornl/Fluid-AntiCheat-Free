7d584410e1cccd463a0331938028811dc3420c50
mistervapornl <64449562+mistervapornl@users.noreply.github.com>
2020-05-26 13:53:54 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -66,3 +66,88 @@ if(checkChatMessage) then
 		end
 	end)
 end
+
+RegisterServerEvent("fluidanticheatkick")
+AddEventHandler("fluidanticheatkick", function(reson)
+	if(IsPlayerAceAllowed(source, "fluidanticheat.bypass")) then
+		if(not reson == "verbodenkeys") then
+			print("FluidAntiCheat: " .. GetPlayerName(source) .. " [" .. source .. "] Heeft permissies om dit uittevoeren.")
+		end
+	else
+		if(GetPlayerName(source)) then
+			TriggerClientEvent("chatMessage", -1, "FluidAntiCheat", {180, 0, 0}, GetPlayerName(source) .. " Is verbannen.")
+		end
+		if(not webhookurl == "") then
+			PerformHttpRequest(webhookurl, function(err, text, headers) end, 'POST', json.encode({username = "FluidAntiCheat", content = GetPlayerName(source) .. " [" .. source .. "] is verbannen."}), { ['Content-Type'] = 'application/json' })
+		end
+		if(reson == "resources") then
+			print()
+			print(GetPlayerName(source) .. " [" .. source .. "] is verbannen omdat de resources zijn veranderd. Identifiers:")
+			for id in pairs(GetPlayerIdentifiers(source)) do
+				print(GetPlayerIdentifiers(source)[id])
+			end
+			print()		
+			DropPlayer(source, "FluidAntiCheat: Je resources zijn veranderd!")
+		elseif(reson == "commands") then
+			print()
+			print(GetPlayerName(source) .. " [" .. source .. "] is verbannen omdat commands veranderd zijn. Identifiers:")
+			for id in pairs(GetPlayerIdentifiers(source)) do
+				print(GetPlayerIdentifiers(source)[id])
+			end
+			print()
+			DropPlayer(source, "FluidAntiCheat: je commands zijn veranderd!")
+		elseif(reson == "godmode") then
+			print()
+			print(GetPlayerName(source) .. " [" .. source .. "] is verbannen, voor het gebruik van godmode. Identifiers:")
+			for id in pairs(GetPlayerIdentifiers(source)) do
+				print(GetPlayerIdentifiers(source)[id])
+			end
+			print()
+			DropPlayer(source, "FluidAntiCheat: er is godmode gedetecteerd!")
+		elseif(reson == "verbodenkeys") then
+			print()
+			print(GetPlayerName(source) .. " [" .. source .. "] is verbannen voor het gebruik van verboden toetsen. Identifiers:")
+			for id in pairs(GetPlayerIdentifiers(source)) do
+				print(GetPlayerIdentifiers(source)[id])
+			end
+			print()
+			DropPlayer(source, "FluidAntiCheat: Je hebt verboden toetsen gebruikt!")
+		elseif(reson == "ontzichtbaarvoertuig") then
+			print()
+			print(GetPlayerName(source) .. " [" .. source .. "] is verbannen omdat voertuig ontzichbaar is. Identifiers:")
+			for id in pairs(GetPlayerIdentifiers(source)) do
+				print(GetPlayerIdentifiers(source)[id])
+			end
+			print()
+			DropPlayer(source, "FluidAntiCheat: Je voertuig is ontzichtbaar!!")
+		elseif(reson == "tp") then
+			print()
+			DropPlayer(source, "FluidAntiCheat: server events zijn veranderd!")
+		elseif(reson == "handling") then
+			print()
+			print(GetPlayerName(source) .. " [" .. source .. "] is verbannen omdat de handling is veranderd. Identifiers:")
+			for id in pairs(GetPlayerIdentifiers(source)) do
+				print(GetPlayerIdentifiers(source)[id])
+			end
+			print()
+			DropPlayer(source, "FluidAntiCheat: Je handling is veranderd!")
+		elseif(reson == "visible") then
+			print()
+			print(GetPlayerName(source) .. " [" .. source .. "] is verbannen omdat hij onzichtbaar was. Identifiers:")
+			for id in pairs(GetPlayerIdentifiers(source)) do
+				print(GetPlayerIdentifiers(source)[id])
+			end
+			print()
+			DropPlayer(source, "FluidAntiCheat: je bent ontzichtbaar!")
+		else
+			print()
+			print(GetPlayerName(source) .. " [" .. source .. "] is verbannen. Identifiers:")
+			for id in pairs(GetPlayerIdentifiers(source)) do
+				print(GetPlayerIdentifiers(source)[id])
+			end
+			print()
+			DropPlayer(source, "FluidAntiCheat: speler is verbannen.")
+		end
+	end
+end)
+
